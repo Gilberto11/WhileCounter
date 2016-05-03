c1239b76350ebdde49d00dd4cf8a0a74d1f6ac47
Gilberto11 <surfcorinthians@hotmail.com>
2016-05-03 17:04:41 +0100

    initial commit

---
+++ src/WhileCounter.java
@@ -0,0 +1,19 @@
+
+public class WhileCounter {
+
+	public static void main(String[] args) {
+		
+		int counter = 1;//declare and initialize control variable
+		
+		while (counter <=10 )//loop - continuation condition
+		{
+			System.out.printf( "%d", counter);
+			++counter;//increment control variable by 1
+			
+		}//end while
+		System.out.println();//output a new line
+		
+		
+	}//end main classWhileCounter
+
+}
